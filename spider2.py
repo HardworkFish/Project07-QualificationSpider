08b9ac5ae8ac2c466a21da0b2dd75d252a142c14
Trsky <625310581@qq.com>
2018-12-08 19:12:41 +0800

    [feature] cehuizizhi spiders

---
+++ spider2.py
@@ -0,0 +1,93 @@
+"""
+Author: TRsky
+Time: 2018-12-07 22:16
+Content: crawl data from cihuizizhi website
+"""
+
+import re
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import pymysql
+
+
+conn = pymysql.connect(
+    host='localhost',  # mysql服务器地址
+    port=3306,  # 端口号
+    user='root',  # 用户名
+    passwd='******',  # 密码
+    db='cehuizizhi',  # 数据库名称
+    charset='utf8',  # 连接编码，根据需要填写
+)
+
+cur = conn.cursor()  # 创建并返回游标
+sqla = '''
+create table if not exists cehuizizhi_gd2(
+        url VARCHAR(100),
+        company VARCHAR(100),
+        qulification_grade varchar(50),
+        address varchar(120),
+        postcode varchar(20),
+        contact varchar(20),
+        phone varchar(20) ,
+        certification varchar(100) ,
+        certification_issuancedate varchar(50),
+        deadline_time varchar(50) ,
+        bisiness text) DEFAULT CHARSET=utf8;
+
+'''
+a = cur.execute(sqla)
+conn.commit()
+
+browser = webdriver.Chrome()
+browser.get('http://chzz.nasg.gov.cn/UnitQuery.aspx')
+# 隐式等待时间
+browser.implicitly_wait(3)
+search_report = browser.find_element_by_id('BtnSearch')
+
+# 匹配 id
+ID_RE = re.compile(r'onclick="ShowInfo\((\d+),1\)"')
+TD_RE = re.compile(r'<td class="tdleft">(.*?)</td>')
+pros_df = ['url', 'company', 'qulification_grade', 'address', 'postcode', 'contact', 'phone', 'certification',
+           'certification_issuancedate',  'deadline_time', 'bisiness']
+
+search_report.click()
+for page in range(1, 151):
+
+    ss = '__doPostBack("PageTurnControl1$ANPager", "%d")' % page
+    browser.execute_script(ss)
+    ids = ID_RE.findall(browser.page_source)
+    print(ids)
+    for id in ids:
+        url = "http://chzz.nasg.gov.cn/PorttalWeb/UnitBaseInfoView.aspx?&ID=" + str(id)
+        print(url)
+        browser.get(url)
+        bps = browser.page_source
+        soup = BeautifulSoup(bps, 'lxml', from_encoding='utf-8')
+        title = soup.find('span', id='lllName')
+        items = soup.find_all('td', class_='tdright')
+        result = dict()
+        result[pros_df[0]] = url
+        result[pros_df[1]] = title.get_text()
+        result[pros_df[2]] = items[0].get_text().strip()
+        result[pros_df[3]] = soup.find('span', id="lblOfficeAddress").get_text()
+        result[pros_df[4]] = items[2].get_text().strip()
+        result[pros_df[5]] = items[3].get_text().strip()
+        result[pros_df[6]] = items[4].get_text().strip()
+        result[pros_df[7]] = items[5].get_text().strip()
+        result[pros_df[8]] = items[6].get_text().strip()
+        result[pros_df[9]] = items[7].get_text().strip()
+        result[pros_df[10]] = items[8].get_text().strip("***\n")
+        print(result)
+        sqla = '''
+                insert into cehuizizhi_gd2(url, company, qulification_grade, address, postcode, contact,
+                                phone, certification, certification_issuancedate,  deadline_time, bisiness)
+                                VALUES (%s,%s,%s,%s,%s, %s,  %s,%s, %s, %s, %s);
+        '''
+        b = cur.execute(sqla, (result[pros_df[0]], result[pros_df[1]], result[pros_df[2]], result[pros_df[3]],
+                               result[pros_df[4]], result[pros_df[5]], result[pros_df[6]], result[pros_df[7]],
+                               result[pros_df[8]], result[pros_df[9]], result[pros_df[10]]))
+        conn.commit()
+    browser.get('http://chzz.nasg.gov.cn/UnitQuery.aspx')
+
+cur.close()
+conn.close()
---
+++ spider3.py
@@ -0,0 +1,50 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import pymysql
+
+conn = pymysql.connect(
+    host='localhost',  # mysql服务器地址
+    port=3306,  # 端口号
+    user='root',  # 用户名
+    passwd='******',  # 密码
+    db='cehuizizhi',  # 数据库名称
+    charset='utf8',  # 连接编码，根据需要填写
+)
+
+cur = conn.cursor()  # 创建并返回游标
+sqla = '''
+create table if not exists cehuizizhi_gd3(
+        name VARCHAR(100),
+        qulification_code varchar(50),
+        type varchar(100),
+        qulification_grade varchar(50),
+        position varchar(50)
+        ) DEFAULT CHARSET=utf8;
+
+'''
+a = cur.execute(sqla)
+conn.commit()
+
+browser = webdriver.Chrome()
+for i in range(1, 24):
+    url = "http://zwfw.nasg.gov.cn/search/zzdw?keyWord=&pageIndex=" + str(i) + "&st=1&dwlx=38&zzdj=4&provice=44"
+    browser.get(url)
+    bps = browser.page_source
+    soup = BeautifulSoup(bps, 'lxml', from_encoding='utf-8')  # 隐式等待时间
+    browser.implicitly_wait(3)
+    result = {}
+    titles = soup.find_all('span', class_='minchen')
+    infos = soup.find_all('span', class_='cang')
+    i = 0
+    for j in range(0, len(infos), 4):
+        print(infos[j].get_text().strip())
+        sqla = '''
+                       insert into cehuizizhi_gd3(name, qulification_code,type,  qulification_grade, position)
+                                       VALUES (%s,%s,%s,%s,%s);
+               '''
+        cur.execute(sqla, (titles[i].get_text().strip(), infos[j].get_text().strip(), infos[j+1].get_text().strip(),
+                           infos[j+2].get_text().strip(), infos[j+3].get_text().strip()))
+        conn.commit()
+        i += 1
+cur.close()
+conn.close()
